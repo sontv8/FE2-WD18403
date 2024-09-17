feb8ff0a25be07df94bafeb1ea7566881510cc21
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-09-17 17:37:03 +0700

    main

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,56 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("blue");
-  const [status, setStatus] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { id: Date.now(), text: inputValue, done: false }]);
+    setInputValue("");
+  };
+
+  const onHandleRemove = (id) => {
+    setTodos(todos.filter((todo) => todo.id != id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id == id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
   return (
     <>
-      {JSON.stringify(status)}
-      <button onClick={() => setColor("orange")}>Change Color</button>
-      <button onClick={() => setStatus(!status)}>Change Status</button>
-      <div style={{ display: status ? "none" : "block" }}>
-        <div style={{ width: 200, height: 200, backgroundColor: color }}>
-          Content
-        </div>
-      </div>
+      {JSON.stringify(todos)}
+      <form action="" onSubmit={onHandleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onInput={(e) => setInputValue(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input type="checkbox" onChange={() => toggleTodo(todo.id)} />
+              <span
+                style={{
+                  textDecoration: todo.done == true ? "line-through" : "none",
+                }}
+              >
+                {todo.text}
+              </span>
+              <button onClick={() => onHandleRemove(todo.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
