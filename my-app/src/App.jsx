94c1d23295a3ce8c431d34599d815d2be1ce4243
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-09-21 18:09:50 +0700

    B5

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,94 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import axios from "axios";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [isEditing, setIsEditing] = useState(null);
-  const [editValue, setEditValue] = useState("");
+  const [products, setProducts] = useState([]);
 
-  const onHandleSubmit = (e) => {
-    e.preventDefault();
-    setTodos([...todos, { id: Date.now(), text: inputValue, done: false }]);
-    setInputValue("");
-  };
-
-  const onHandleRemove = (id) => {
-    setTodos(todos.filter((todo) => todo.id != id));
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id == id ? { ...todo, done: !todo.done } : todo
-      )
-    );
-  };
-
-  const onHandleEditClick = (todo) => {
-    setIsEditing(todo);
-  };
-  const onHandleSaveEdit = (e) => {
-    e.preventDefault();
-    setTodos(
-      todos.map((todo) =>
-        todo.id == isEditing.id ? { ...todo, text: editValue } : todo
-      )
-    );
-    setIsEditing(null);
-  };
-  const onHandleCancelSave = () => {
-    setIsEditing(null);
-  };
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(`http://localhost:3000/products`);
+      setProducts(response.data);
+    })();
+  }, []);
   return (
     <>
-      {JSON.stringify(todos)}
-      <form action="" onSubmit={onHandleSubmit}>
-        <input
-          type="text"
-          value={inputValue}
-          onInput={(e) => setInputValue(e.target.value)}
-        />
-        <button>Add</button>
-      </form>
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <input type="checkbox" onChange={() => toggleTodo(todo.id)} />
-              {isEditing && isEditing.id == todo.id ? (
-                <>
-                  <form action="" onSubmit={onHandleSaveEdit}>
-                    <input
-                      type="text"
-                      defaultValue={isEditing.text}
-                      onInput={(e) => setEditValue(e.target.value)}
-                    />
-                    <button>Save</button>
-                    <button onClick={onHandleCancelSave}>Cancel</button>
-                  </form>
-                </>
-              ) : (
-                <>
-                  <span
-                    style={{
-                      textDecoration:
-                        todo.done == true ? "line-through" : "none",
-                    }}
-                    onClick={() => onHandleEditClick(todo)}
-                  >
-                    {todo.text}
-                  </span>
-                  <button onClick={() => onHandleRemove(todo.id)}>
-                    Delete
-                  </button>
-                </>
-              )}
-            </li>
-          );
-        })}
-      </ul>
+      <h1>Danh sách sản phẩm</h1>
+      {products.map((product) => (
+        <div key={product.id}>
+          {product.name} - {product.price}
+        </div>
+      ))}
     </>
   );
 }
