aec4a04c6cb24020b3e6148001f906766c2d8f28
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-10-01 16:55:46 +0700

    Delete

--- my-app/src/components/ProductList.jsx
+++ my-app/src/components/ProductList.jsx
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
 
 const ProductList = () => {
+  const queryClient = useQueryClient();
   const { isLoading, isError, data, error } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
@@ -10,6 +11,18 @@ const ProductList = () => {
       return response.data;
     },
   });
+
+  const mutation = useMutation({
+    mutationFn: async (id) => {
+      await axios.delete(`http://localhost:3000/products/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["products"],
+      });
+    },
+  });
+  // console.log(mutation); //{mutate: () => {}}
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>{error.message}</div>;
@@ -19,11 +32,17 @@ const ProductList = () => {
       {data.map((product) => (
         <div key={product.id}>
           {product.name} - {product.price}
-          <button>Xoa</button>
+          <button onClick={() => mutation.mutate(product.id)}>Xoa</button>
         </div>
       ))}
     </div>
   );
 };
+/*
+  B1: bắt sự kiện onClick khi click vào button
+  B2: khi sự kiện onClick được kích hoạt thì sẽ gọi ra 1 hàm để thực thi
+  B3: trong hàm này sẽ gọi ra API để xóa sản phẩm
+  B4: cập nhật lại data và render lại
+*/
 
 export default ProductList;
