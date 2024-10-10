09e9b3b11ef1b6cccaa15a9d3f5c1386dea3b5c1
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-10-10 16:57:47 +0700

    upload image

--- my-app/src/pages/AddProduct.jsx
+++ my-app/src/pages/AddProduct.jsx
@@ -1,11 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
-import { Button, Form, Input, InputNumber, Select, Switch } from "antd";
+import { Button, Form, Input, InputNumber, Select, Switch, Upload } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { PlusOutlined } from "@ant-design/icons";
 
 const AddProduct = () => {
+  const [imageUrl, setImageUrl] = useState("");
   const navigate = useNavigate();
   const { mutate } = useMutation({
     mutationFn: async (product) => {
@@ -15,8 +17,22 @@ const AddProduct = () => {
       navigate("/admin/products");
     },
   });
+
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
+  const onUploadChange = (info) => {
+    if (info.file.status == "done") {
+      setImageUrl(info.file.response.secure_url);
+    }
+  };
+
   const onFinish = (values) => {
-    console.log(values);
+    mutate({ ...values, imageUrl });
   };
   return (
     <div>
@@ -60,16 +76,33 @@ const AddProduct = () => {
           <InputNumber />
         </Form.Item>
         <Form.Item
-          label="Ảnh sản phẩm"
-          name="imageUrl"
-          rules={[
-            {
-              required: true,
-              message: "Vui lòng nhập ảnh sản phẩm",
-            },
-          ]}
+          label="Upload"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
         >
-          <Input />
+          <Upload
+            action="https://api.cloudinary.com/v1_1/dxhgeg7vi/image/upload"
+            listType="picture-card"
+            data={{ upload_preset: "upload-demo" }}
+            onChange={onUploadChange}
+          >
+            <button
+              style={{
+                border: 0,
+                background: "none",
+              }}
+              type="button"
+            >
+              <PlusOutlined />
+              <div
+                style={{
+                  marginTop: 8,
+                }}
+              >
+                Upload
+              </div>
+            </button>
+          </Upload>
         </Form.Item>
         <Form.Item label="Trạng thái" name="available" initialValue={false}>
           <Switch />
